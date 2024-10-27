c26c23de8b5c8e3c0e1b656fdf5a7bf475178fe7
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-10-27 19:04:27 +0100

    Solution of the task with id 1302

---
+++ src/main/kotlin/task1322.kt
@@ -0,0 +1,41 @@
+package org.example
+
+import kotlin.math.absoluteValue
+
+/**
+ * Class with the solution to problem 1322
+ * @property array List<Int> The array to be filtered
+ * @property filteredElements List<Int> The elements with an even digit sum
+ */
+class EvenDigitSumFilter(private val array: List<Int>) {
+    // lambda to calculate digit sum
+    private val digitSum: (Int) -> Int = { num -> num.absoluteValue.toString().map { it.digitToInt() }.sum() }
+
+    // filter the elements with an even digit sum
+    val filteredElements: List<Int> = array.filter { digitSum(it) % 2 == 0 }
+}
+
+
+/**
+ * Main program
+ */
+fun main() {
+    val result = try {
+        val n = readLine()!!.toInt()
+        val inputArray = readLine()!!.split(" ").map { it.toInt() }
+
+        // check if the array size matches n
+        if (n != inputArray.size) throw IllegalArgumentException("Array size does not match n")
+
+        // Create an instance and get filtered output
+        val filter = EvenDigitSumFilter(inputArray)
+        val count = filter.filteredElements.size
+        val output = if (count > 0) filter.filteredElements.joinToString(" ") else null
+        "$count\n${output ?: ""}".trim()
+    } catch (e: Exception) {
+        "Error: ${e.message}"
+    }
+
+    // result
+    println(result)
+}
---
+++ src/test/kotlin/TestTask1322.kt
@@ -0,0 +1,39 @@
+import org.example.EvenDigitSumFilter
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Tests for Task 1322
+ */
+class TestTask1322 {
+    @Test
+    fun testExample1() {
+        // Input data
+        val array = listOf(1, 10, -12, 404, 5030, 10000, -321)
+
+        // Run the class for Task 1322
+        val filter = EvenDigitSumFilter(array)
+
+        // Expected output
+        val expectedOutput = listOf(404, 5030, -321)
+
+        // Check the result
+        assertEquals(expectedOutput, filter.filteredElements)
+    }
+
+    @Test
+    fun testExample2() {
+        // Input data
+        val array = listOf(11, 0, 1, 10, -20, -30900, -300, 3010)
+
+        // Run the class for Task 1322
+        val filter = EvenDigitSumFilter(array)
+
+        // Expected output
+        val expectedOutput = listOf(11, 0, -20, -30900, 3010)
+
+        // Check the result
+        assertEquals(expectedOutput, filter.filteredElements)
+    }
+
+}
